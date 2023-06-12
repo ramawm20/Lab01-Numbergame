344cdce30343c171a11858c08bbca4246ee65aec
ramawm20 <ramawael54@gmail.com>
2023-06-12 13:46:29 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Numbergame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
